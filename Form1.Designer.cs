d992d74bbd55f6c337bbd9455704c8364f428adb
user <user@DJLEE25>
2025-08-08 17:49:50 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace TimerComparisonForm
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblWF;
+        private System.Windows.Forms.Label lblTimers;
+        private System.Windows.Forms.Label lblThreading;
+        private System.Windows.Forms.Button btnBlockUI;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblWF = new System.Windows.Forms.Label();
+            this.lblTimers = new System.Windows.Forms.Label();
+            this.lblThreading = new System.Windows.Forms.Label();
+            this.btnBlockUI = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // lblWF
+            this.lblWF.AutoSize = true;
+            this.lblWF.Location = new System.Drawing.Point(12, 9);
+            this.lblWF.Name = "lblWF";
+            this.lblWF.Size = new System.Drawing.Size(111, 13);
+            this.lblWF.TabIndex = 0;
+            this.lblWF.Text = "WinForms.Timer: 0";
+
+            // lblTimers
+            this.lblTimers.AutoSize = true;
+            this.lblTimers.Location = new System.Drawing.Point(12, 35);
+            this.lblTimers.Name = "lblTimers";
+            this.lblTimers.Size = new System.Drawing.Size(119, 13);
+            this.lblTimers.TabIndex = 1;
+            this.lblTimers.Text = "System.Timers.Timer: 0";
+
+            // lblThreading
+            this.lblThreading.AutoSize = true;
+            this.lblThreading.Location = new System.Drawing.Point(12, 61);
+            this.lblThreading.Name = "lblThreading";
+            this.lblThreading.Size = new System.Drawing.Size(133, 13);
+            this.lblThreading.TabIndex = 2;
+            this.lblThreading.Text = "System.Threading.Timer: 0";
+
+            // btnBlockUI
+            this.btnBlockUI.Location = new System.Drawing.Point(12, 90);
+            this.btnBlockUI.Name = "btnBlockUI";
+            this.btnBlockUI.Size = new System.Drawing.Size(150, 30);
+            this.btnBlockUI.TabIndex = 3;
+            this.btnBlockUI.Text = "UI 5초 멈춤 시작";
+            this.btnBlockUI.UseVisualStyleBackColor = true;
+            this.btnBlockUI.Click += new System.EventHandler(this.btnBlockUI_Click);
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(284, 131);
+            this.Controls.Add(this.btnBlockUI);
+            this.Controls.Add(this.lblThreading);
+            this.Controls.Add(this.lblTimers);
+            this.Controls.Add(this.lblWF);
+            this.Name = "Form1";
+            this.Text = "Timer Comparison with UI Block";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Timers;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace TimerComparisonForm
+{
+    public partial class Form1 : Form
+    {
+        private System.Windows.Forms.Timer wfTimer;
+        private System.Timers.Timer timersTimer;
+        private System.Threading.Timer threadingTimer;
+
+        private int wfCount = 0;
+        private int timersCount = 0;
+        private int threadingCount = 0;
+
+        private bool isBlocked = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitTimers();
+        }
+
+        private void InitTimers()
+        {
+            // WinForms.Timer (UI 스레드 기반)
+            wfTimer = new System.Windows.Forms.Timer();
+            wfTimer.Interval = 100;
+            wfTimer.Tick += (s, e) =>
+            {
+                wfCount++;
+                lblWF.Text = $"WinForms.Timer: {wfCount}";
+            };
+            wfTimer.Start();
+
+            // System.Timers.Timer (백그라운드)
+            timersTimer = new System.Timers.Timer(100);
+            timersTimer.Elapsed += (s, e) =>
+            {
+                this.Invoke((Action)(() =>
+                {
+                    timersCount++;
+                    lblTimers.Text = $"System.Timers.Timer: {timersCount}";
+                }));
+            };
+            timersTimer.Start();
+
+            // System.Threading.Timer (스레드풀)
+            threadingTimer = new System.Threading.Timer(_ =>
+            {
+                this.Invoke((Action)(() =>
+                {
+                    threadingCount++;
+                    lblThreading.Text = $"System.Threading.Timer: {threadingCount}";
+                }));
+            }, null, 0, 100);
+        }
+
+        private void btnBlockUI_Click(object sender, EventArgs e)
+        {
+            if (isBlocked) return;
+            isBlocked = true;
+            wfCount = timersCount = threadingCount = 0;
+            lblWF.Text = lblTimers.Text = lblThreading.Text = "Starting...";
+
+            // UI 스레드 5초간 블로킹
+            StopwatchBlock(5000);
+
+            isBlocked = false;
+        }
+
+        private void StopwatchBlock(int milliseconds)
+        {
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+            while (sw.ElapsedMilliseconds < milliseconds)
+            {
+                // 의도적 블로킹
+                //Application.DoEvents(); // 넣으면 UI 응답 유지, 빼면 완전 멈춤
+            }
+            sw.Stop();
+        }
+    }
+}
