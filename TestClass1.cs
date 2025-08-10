b5e4b5f3ea4d9dad82d095b9deefbb211fef7d95
user12 <user@DJLEE25>
2025-08-11 08:40:15 +0900

    TestClass plus

---
+++ TestClass1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimerComparisonForm
+{
+    class TestClass1
+    {
+        //이것도 추가
+    }
+}
